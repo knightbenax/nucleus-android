d9cf66d1d39d08d66f973b4247b078e345b559bb
Bezaleel Ashefor <knightbenax@gmail.com>
2017-11-17 21:01:07 +0100

    Rap Battle

--- app/src/main/java/com/ydiworld/nucleus/EventFrag.java
+++ app/src/main/java/com/ydiworld/nucleus/EventFrag.java
@@ -3,13 +3,11 @@ package com.ydiworld.nucleus;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-
-
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 /**
  * Created by sammy on 11/11/17.
